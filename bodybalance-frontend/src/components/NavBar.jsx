4064026f0975431680013a19f816d79e49292b15
Shravan Suvarna <21shravansuvarna@gmail.com>
2025-05-01 20:59:01 +0530

    navbar

--- bodybalance-frontend/src/components/NavBar.jsx
+++ bodybalance-frontend/src/components/NavBar.jsx
@@ -1,8 +1,10 @@
 import {  Route, Routes, Link } from "react-router-dom";
 export default function NavBar(){
     return(
-        <div>
-        <Link to="/register">signup</Link>
+      <div className="max-w-md mx-auto p-4 bg-white rounded shadow">
+        <Link to="/register"><button className= "text-gray px-4 py-2 rounded mt-2 hover:bg-blue-800">
+    SignUp
+  </button></Link>
       </div>
     )
 }
