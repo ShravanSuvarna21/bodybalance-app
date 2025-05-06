aeca5a52549f5c9ead459ef4ea72861436e50822
Shravan Suvarna <21shravansuvarna@gmail.com>
2025-05-06 21:31:23 +0530

    database

--- bodybalance-backend/config/database.js
+++ bodybalance-backend/config/database.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 const configDB=async()=>{
     try{
         mongoose.connect(process.env.DB_URL)
+        console.log("DB is connected")
         }catch(err){
         }
 }
