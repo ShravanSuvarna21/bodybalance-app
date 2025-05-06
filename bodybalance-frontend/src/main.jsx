70f6ca5b682226ede3e421812b5aca3990c8b2fb
Shravan Suvarna <21shravansuvarna@gmail.com>
2025-05-06 21:33:35 +0530

    main

--- bodybalance-frontend/src/main.jsx
+++ bodybalance-frontend/src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
-// import store from './store/configureStore.jsx';
-// import { Provider } from 'react-redux';
+import store from './store/configureStore.jsx';
+import { Provider } from 'react-redux';
 createRoot(document.getElementById('root')).render(
-  // <Provider store={store}>
+  <Provider store={store}>
   <BrowserRouter>
      <App/>
     </BrowserRouter>
-  // </Provider>
+  </Provider>
 
 )
